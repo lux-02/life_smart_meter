1716ade6618832ef5be0c699940f0e993704ee6f
lux00 <oys128950@gmail.com>
2025-03-06 02:35:12 +0900

    apply to googleAnalytics

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-624VE81P8V"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-624VE81P8V');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
